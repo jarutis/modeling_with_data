34546759cefd281175fcdf912da3823d9a16e11b
Jonas Jarutis <jonas@vinted.com>
2016-03-11 20:52:09 +0200

    Add 2.16 exercise

---
+++ q_2_16/astrncpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void astrcpy(char **base, char *copyme) {
+  *base = realloc(*base, sizeof(char) * strlen(copyme));
+  strcpy(*base, copyme);
+}
+
+void astrcat(char **base, char *copyme) {
+  int new_length = strlen(*base) + strlen(copyme);
+  *base = realloc(*base, sizeof(char) * new_length);
+  strcat(*base, copyme);
+}
+
+int main(void) {
+  char *container = NULL;
+  char text[] = "Once upon a time in a dark night something happened";
+  char other_text[] = " and happened some more";
+  astrcpy(&container, text);
+  printf("Copied: %s\n", container);
+  astrcat(&container, other_text);
+  printf("Concatenated: %s\n", container);
+  return 0;
+}
